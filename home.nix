9918f3b668dd1fa0ece64c4c47e4d895e0feadf4
rida-hdj <rico777x@gmail.com>
2026-01-03 19:36:46 +0100

    Update home.nix

--- home.nix
+++ home.nix
@@ -12,20 +12,3 @@
   #...
   ];
 }
-
- ~ 
-󱞩  cat /etc/nixos/home.nix
-{ config, pkgs, ... }:
-
-{
-  home.username = "rida";
-  home.homeDirectory = "/home/rida";
-
-  home.stateVersion = "25.11";
-  
-  programs.home-manager.enable = true;
-
-  home.packages = with pkgs; [
-  #...
-  ];
-}
