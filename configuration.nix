d21c6d80946316f8c3d9f327f328b1bd220517fd
rida-hdj <rico777x@gmail.com>
2026-01-03 19:28:50 +0100

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
     
 {
@@ -10,37 +6,49 @@
       ./hardware-configuration.nix
     ];
 
-hardware.graphics.enable32Bit = true;
+  hardware.graphics.enable32Bit = true;
+  
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+
+  #enable plymouth
+  boot.plymouth.enable = true;
+
+  boot.initrd.systemd.enable = true;
+
+  boot.plymouth.theme = "bgrt";
+
+  boot.initrd.verbose = false;
+  boot.consoleLogLevel = 0;
+  boot.kernelParams = [ "quiet" "splash" ];
+
+  #kernel version
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
   #--enable nixos flakes--
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.hostName = "nixos";
+  
+  #enable niri
+  programs.niri.enable = true;
+  xdg.portal.enable = true;
+
+  #enable noctalia
+  services.noctalia-shell.enable = true;
+
   #enable fish shell
   users.users.rida.shell = pkgs.fish;
   programs.fish.enable = true;
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Enable networking
   # Set your time zone.
   time.timeZone = "Africa/Casablanca";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
-  # Enable the X11 windowing system.
-#  services.xserver.enable = true;
-  # Enable niri
-#programs.niri.enable = true;
-#programs.hyprland.xwayland.enable = true;
-#programs.hyprland.withUWSM  = true;
-#environment.sessionVariables.NIXOS_OZONE_WL = "1";
-  # Enable the kde plasma Desktop Environment.
+  # Enable DE & LM
   services.displayManager.ly.enable = true;
   services.desktopManager.plasma6.enable = true;
 
@@ -48,7 +56,6 @@ hardware.graphics.enable32Bit = true;
 zramSwap = {
   enable = true;
   algorithm = "zstd";
-  # This refers to the uncompressed size, actual memory usage will be lower.
   memoryPercent = 80;
 };
 
@@ -62,8 +69,6 @@ services.xserver = {
   };
 };
 
-  services.gvfs.enable = true;
-  services.tumbler.enable = true;
   services.fwupd.enable = true;
 
 systemd.user.services.setxkbmap = {
@@ -86,21 +91,8 @@ systemd.user.services.setxkbmap = {
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-#nixpkgs.config.permittedInsecurePackages = [
-#  "mbedtls-2.28.10"
-#];
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  
   users.users.rida = {
     isNormalUser = true;
     description = "rida";
@@ -108,17 +100,30 @@ systemd.user.services.setxkbmap = {
     packages = with pkgs; [
 
 librewolf
+brave
 thunderbird
 libreoffice
+nerd-fonts.jetbrains-mono
 neovim
 neovide
 helix
+vscodium
 tmux
+starship
 htop
 btop
 fastfetch
 git
+fd
+lsd
+clock-rs
+gnome-calculator
 nodejs
+lua-language-server
+pyright
+rust-analyzer
+nodePackages.vscode-langservers-extracted
+nodePackages.typescript-language-server
 kitty
 wget
 uget
@@ -152,21 +157,33 @@ gthumb
 nerd-fonts.fira-code
 cmatrix
 lazygit
+yazi
+termusic
 ncdu
 networkmanager
 bat
 vicinae
+adwaita-icon-theme
+xwayland-satellite
+alsa-utils
+xdg-desktop-portal-gnome
+xdg-desktop-portal-gtk
+wl-clipboard-x11
+wl-clipboard
+#jellyfin-media-player
+jellyfin-ffmpeg
 ];
 };
   # Install firefox.
   programs.firefox.enable = true;
   programs.nix-ld.enable = true;
-
+  #enable appimage.
   programs.appimage = {
  enable = true;
  binfmt = true;
  };
 
+  #enable kde connect.
 programs.kdeconnect.enable = true;
 networking.firewall.allowedTCPPorts = [ 1714 1715 1716 1717 1718 1719 ];
 networking.firewall.allowedUDPPorts = [ 1714 1715 1716 1717 1718 1719 ];
@@ -174,10 +191,7 @@ networking.firewall.allowedUDPPorts = [ 1714 1715 1716 1717 1718 1719 ];
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
-  adwaita-icon-theme
   ];
 
 environment.pathsToLink = [ "/share/icons" "/share/applications" ];
@@ -186,38 +200,21 @@ environment.pathsToLink = [ "/share/icons" "/share/applications" ];
   GSK_RENDERER = "ngl";
 
 };
+  #enable steam
 programs.steam = {
   enable = true;
-  remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-  dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-  localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  remotePlay.openFirewall = true;
+  dedicatedServer.openFirewall = true; 
+  localNetworkGameTransfers.openFirewall = true; 
 };
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  #enable jellyfin
+  services.jellyfin = {
+    enable = true;
+    openFirewall = true;
+  };
+  systemd.services.jellyfin.wantedBy = [];
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.05"; # Did you read the comment?
+  system.stateVersion = "25.05";
 
 }
