dacf2067967fc067a53747951de18777b6bbabc5
rida-hdj <rico777x@gmail.com>
2026-02-28 22:50:01 +0000

    Add session variables and new packages to home.nix

--- home.nix
+++ home.nix
@@ -1,14 +1,35 @@
-{ config, pkgs, ... }:
+{
+  config,
+  pkgs,
+  inputs,
+  ...
+}:
 
 {
   home.username = "rida";
   home.homeDirectory = "/home/rida";
 
   home.stateVersion = "25.11";
-  
+
   programs.home-manager.enable = true;
+  home.sessionVariables = {
+  };
+  imports = [
+    ./modules/fish.nix
+    ./modules/nh.nix
+    ./modules/neovim.nix
+  ];
 
   home.packages = with pkgs; [
-  #...
+    nerd-fonts.jetbrains-mono
+    papirus-icon-theme
+    nil
+    lua-language-server
+    bash-language-server
+    marksman
+    typescript-language-server
+    vscode-langservers-extracted
+    pyright
+    emmet-ls
   ];
 }
